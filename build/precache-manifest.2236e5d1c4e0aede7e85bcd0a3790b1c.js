94baac45f665f76a4e3be62f819e77d66c4cc711
Marcos Paulo <marcosbarretopaulo@gmail.com>
2021-10-31 15:17:00 -0300

    first commit

---
+++ build/precache-manifest.2236e5d1c4e0aede7e85bcd0a3790b1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437f30644839824c362a9c72e35f1276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f2877e723cefe0538ff",
+    "url": "/static/css/main.68ae57bb.chunk.css"
+  },
+  {
+    "revision": "5eb6d4cfd86eca0d88c9",
+    "url": "/static/js/2.97240464.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.97240464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2877e723cefe0538ff",
+    "url": "/static/js/main.14d76a77.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "83252e0afaf5f134063345abd6ac8fd7",
+    "url": "/static/media/background.83252e0a.jpg"
+  }
+]);
